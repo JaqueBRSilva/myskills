5b784e682f0630570d20afe919655f634fc8baa5
JaqueBRSilva <jaqueline.broccolo@softvision.com.br>
2021-12-26 20:47:21 -0300

    refactor: atualiz. componente 'button' p/ typescript

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { 
+    StyleSheet,
+    Text, 
+    TouchableOpacity, 
+    TouchableOpacityProps 
+} from 'react-native';
 
+interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+}
 
-export function Button({ onPress }) {
+export function Button({ title, ...rest } : ButtonProps ) {
 
     return (
         <TouchableOpacity
             style={styles.button}
             activeOpacity={.7}
-            onPress={onPress}
+            {...rest}
         >
-            <Text style={styles.buttonText}> Add </Text>
+            <Text style={styles.buttonText}>
+                { title }
+            </Text>
         </TouchableOpacity>
     )
 }

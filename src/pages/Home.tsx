2b17723a3fd3a37b73b40cb078434c2200136fb6
JaqueBRSilva <jaqueline.broccolo@softvision.com.br>
2021-12-26 20:52:44 -0300

    refactor: atualiz. page 'home' p/ typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,18 +10,24 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+   id: string;
+   name: string;
+}
 
 export function Home() {
 
    const [newSkill, setNewSkill] = useState('');
-   const [mySkills, setMySkills] = useState([]);
-
+   const [mySkills, setMySkills] = useState<SkillData[]>([]);
+   const [grettng, setGretting] = useState('');
 
    function handleAddNewSkill() {
-      setMySkills(oldState => [...oldState, newSkill]);
+      const data = {
+         id: String(new Date().getTime()),
+         name: newSkill
+      }
+      setMySkills(oldState => [...oldState, data]);
    }
-
-
 
    return (
       <View style={styles.container}>
@@ -37,7 +43,10 @@ export function Home() {
             onChangeText={setNewSkill}
          />
 
-         <Button onPress={handleAddNewSkill} />
+         <Button 
+            title="Add"
+            onPress={handleAddNewSkill}
+         />
 
          <Text style={[styles.title, { marginVertical: 50 }]}>
             My Skills
@@ -45,12 +54,11 @@ export function Home() {
 
          <FlatList
             data={mySkills}
-            keyExtractor={item => item}
+            keyExtractor={item => item.id}
             renderItem={({ item }) => (
-               <SkillCard skill={item} />
+               <SkillCard skill={item.name} />
             )}
          />
-
 
       </View>
    )
@@ -60,9 +68,8 @@ const styles = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: '#121015',
-      paddingHorizontal: 20,
+      paddingHorizontal: 30,
       paddingVertical: 70,
-      paddingHorizontal: 30
    },
 
    title: {
@@ -79,5 +86,4 @@ const styles = StyleSheet.create({
       marginTop: 30,
       borderRadius: 7
    }
-
 })
